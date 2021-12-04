aaa6017caf309daf3233f5c422ecec6116c1510a
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:33:42 +0530

    Add files via upload

---
+++ total number.py
@@ -0,0 +1,12 @@
+# salary=int(input("enter the salary"))
+# notes=int(input("enter the notes"))
+# if salary==1000:
+#     print(salary//notes)
+# elif salary==2000:
+#     print(salary//notes)
+# elif salary==3000:
+#     print(salary//notes)
+# elif salary==4000:
+#     print(salary//notes)
+# else:
+#     print("nothing")
