e1afd83cbced56e53a71cdf392d66cf73daecedc
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:45:08 +0530

    Add files via upload

---
+++ oldest_youngest_younger.py
@@ -0,0 +1,61 @@
+
+# ###MAXIMUM NUMBER
+
+# age1=int(input("enter the age1:"))
+# age2=int(input("enter the age2:"))
+# age3=int(input("enter the age3:"))
+# if age1>age2 and age1>age3:
+#     print(age1,"oldest")
+# elif age1<age2 and age2 >age3:
+#     print(age2,"oldest")
+# elif age3>age1 and age3>age2:
+#     print(age3,"oldest")
+# else:
+#     print("same age")
+
+# if age1>age2 and age2>age3 :
+#     print(age1,"is oldest")
+#     print(age2,"is younger")
+#     print(age3,"is youngest")
+# elif age2>age1 and age1>age3:
+    
+
+
+# ###MINIMUM NUMBER
+# a=int(input("enter the number:"))
+# b=int(input("enter the numbe:"))
+# c=int(input("enter the number:"))
+# if a<b and a<c:
+#     print(a," is a minimum number")
+# elif b<c and b<a:
+#     print(b," is a minimum number")
+# elif c<a and c<b:
+#     print(c," is a minimum number")
+# elif a==b==c:
+#     print(" the numbers are equal")
+# else:
+#     print(" maybe 2 equal minimum numbers ")
+
+
+# ##MEDIAN NUMBER
+a=int(input("enter any number:"))
+b=int(input("enter any number:"))
+c=int(input("enter any number:"))
+# if a<b and b<c:
+#     print(b,"median number")
+# elif b<a and a<c:
+#     print(a,"median number")
+# elif a<c and c<b:
+#     print(c,"median number")
+# elif a==b==c:
+#     print("equal")
+# else:
+#     print(" has two median numbers")
+
+if a>b and a>c:
+    print(a,"a is oldest")
+elif a>b and b>c:
+    print(b,"b is middle one")
+else:
+    print(c,"c is smaller")
+
