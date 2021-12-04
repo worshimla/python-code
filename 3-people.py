2e16af70d5429f81fe02cf651c52531c649c8b18
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 22:10:46 +0530

    Add files via upload

---
+++ 3-people.py
@@ -0,0 +1,17 @@
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# c=int(input("enter the number:"))
+# if a>b and a>c:
+#     print("a is oldest")
+# elif a<b and a<c:
+#     print("a is youngest")
+# if b>a and b>c:
+#     print("b is oldest")
+# elif b<a and b<c:
+#     print("b is youngest")
+# if c>a and c>b:
+#     print("c is oldest")
+# elif c<a and c<b:
+#     print("c is youngest")
+# else:
+#     print("among")
