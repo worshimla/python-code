3c7777007aea12623bd9b69d2b37411c37db4451
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:40:15 +0530

    Add files via upload

---
+++ side_ triangle.py
@@ -0,0 +1,7 @@
+# num1=int(input("enter the num"))
+# num2=int(input("enter the num"))
+# num3=int(input("enter te num"))
+# if num1+num2+num3==180:
+#     print("traingle")
+# else:
+#     print("not triangle")
