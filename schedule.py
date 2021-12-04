9eee6dd08a5d00e1c52d47a3bf6df183f91ef388
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:41:07 +0530

    Add files via upload

---
+++ schedule.py
@@ -0,0 +1,20 @@
+# time= input("enter the time:")
+# if time>"7:00am" and time<="7:59am":
+#     print("breakfast")
+# elif time>"8:00am"or time<="12:29pm":
+#     print("first coding")
+# elif time>"12:30pm" and time<="13:59pm":
+#     print("lunch")
+# elif time>"14:00pm" and time<="16:29pm":
+#     print("second coding")
+# elif time>"16:30pm" and time<="16:59pm":
+#     print("exercise")
+# elif time>"17:00pm" and time<="17:59pm":
+#     print("snacks time")
+# elif time>"18:00pm" and time<="19:59pm":
+#     print("english activity")
+# elif time>"20:00pm"and time<="20:59pm":
+#     print("dinner")
+# else:
+#     print("sleeping")
+
