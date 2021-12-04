34441a3992cf74ac920aa7c42f51b1b3d886fab2
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:33:09 +0530

    Add files via upload

---
+++ upper_lower.py
@@ -0,0 +1,8 @@
+# ch=(input("enter the character"))
+# if ch>='a'and ch<='b':
+#     print("it is lower case")
+# elif ch>='A'and ch>='B':
+#     print("it is upper case")
+# else:
+#     print("both are not")
+
