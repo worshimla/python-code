92b1d00308e40cf43725676c1b1008f17f569202
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:32:28 +0530

    Add files via upload

---
+++ weather_alphabet_digit.py
@@ -0,0 +1,20 @@
+# ch=input("enter the letter")
+# if ch in"a to z" or ch in"A toZ":
+#     print("alphbet")
+# elif ch in "0 to 9":
+#     print("digit")
+# else:
+#     print("special character")
+
+
+
+
+# ch=input("enter the letter")
+# num=int(input("enter the number"))
+# ch=input(input("enter the character"))
+# if ch=="a" and ch=="z":
+#     print("alphabet")
+# elif ch>="0" and ch<="9":
+#     print("digit")
+# else:
+#     print("special character")
