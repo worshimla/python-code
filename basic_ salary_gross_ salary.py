ad1a8e8eb22b4700a15f352f4627419ec896a5f3
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 22:06:39 +0530

    Add files via upload

---
+++ basic_ salary_gross_ salary.py
@@ -0,0 +1,12 @@
+# BS=int(input("enter the basic salary:"))
+# GS=int(input("enter the gross salary:"))
+# if BS<=10000:
+#     a=BS-GS
+#     print(a,"HRA=20%,DA=80%")
+# elif BS<=20000:
+#     b=GS-BS
+#     print(b,"HRA=25%DA=90%")
+# elif BS>20000:
+#     print("HRA=30%,DA=95%")
+# else:
+#     print("no ")
