6c51c99d99cab2235d8c0d7cfb8bfc5f9ee6c279
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:44:49 +0530

    Add files via upload

---
+++ phy_chem_bio.py
@@ -0,0 +1,19 @@
+phy=int(input("enter the percentage:"))
+# chem=int(input("enter the percentege:"))
+# bio=int(input("enter the percentege:"))
+# maths=int(input("enter the percentage:"))
+# computer=int(input("enter the percentage:"))
+# total=phy+chem+bio+maths+computer
+# percentage=total/5
+# if percentage >=90:
+#     print("grade a")
+# elif percentage >=80:
+#     print("grate b")
+# elif percentage >=70:
+#     print("grate c")
+# elif percentage >=60:
+#     print("grate d")
+# elif percentage >=40:
+#     print("grate e")
+# else:
+#     print("grate f")
