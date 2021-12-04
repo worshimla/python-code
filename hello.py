5c42bb266e71418a3c3e4890440626398e13fbea
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:51:35 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,25 @@
+# num=int(input("enter the num:"))
+# if num>=5 and num<=10:
+#     # if num>=5 and num<=10:
+#     print("hello")
+# elif num>=10 and num<=20:
+#     # if num>=10 and num<=20:
+#     print("i am kabita")
+# elif num>=20 and num<=30:
+#     # if num>=20 and num<=30:
+#     print("i am worshim")
+# else:
+#     print("nothing")
+
+num=int(input("enter the num:"))
+if num==num:
+    if num>5:
+        print("hello")
+    if num>10:
+        if num<20:
+            print(" i am kabita")
+    if num >20:
+        if num<30:
+            print(" i am worshim")
+else:
+    print("nothing")
