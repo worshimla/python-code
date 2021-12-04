15a931d1c137a1ac78faacc923c7b0a0758beb2e
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:47:24 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,9 @@
+# a=int(input("enter the first number:-"))
+# b=int (input("enter the second no:-"))
+# c=int(input("enter the second no:-"))
+# if a>=b and a>=c:
+#     print("a is greater")
+# elif b>=a and b>=c:
+#     print("b is greater")
+# else:
+#     print("c is greater")
