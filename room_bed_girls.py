6e0a41d9725ed6805e997bb6b820ea4bfdca9507
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:41:38 +0530

    Add files via upload

---
+++ room_bed_girls.py
@@ -0,0 +1,98 @@
+room=int(input("enter the room no:"))
+girls=int(input("enter the girls no:"))
+bed=int(input("enter the bed no:"))
+if room==104:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==103:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==201:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==203:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==301:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==302:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==303:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==304:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==401:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==402:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==403:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==404:
+    if bed>girls:
+        print(bed-girls,"shift girls in that room")
+    elif bed==girls:
+        print("full girls and bed no empty place")
+    elif girls>bed:
+        print(girls-bed,"girls is more bed is less if in room empty place so u can shift bed")
+elif room==204:
+    print("team member room")
+elif room==202:
+    print("kitchen ")
+elif room==102:
+    print("apperentis room")
+elif room==101:
+    print("program manager")
+else:
+    print("nothing")
+
