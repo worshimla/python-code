ff96fb85fde4d2124324014e472acac7c79f0b53
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:36:49 +0530

    Add files via upload

---
+++ string-int.py
@@ -0,0 +1,10 @@
+# num=int(input("enter the num:"))
+num=(input("enter the string:"))
+if num==1:
+    print("it is number")
+elif num=="kabita":
+    print("it is string")
+elif num==1.24:
+    print("it is float")
+else:
+    print("it is compiler")
