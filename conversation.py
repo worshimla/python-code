6132ac026e175002d79a21e6e28c80882c6ca81d
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 22:05:54 +0530

    Add files via upload

---
+++ conversation.py
@@ -0,0 +1,93 @@
+# a=100
+# b=99
+# if a>b:
+#     print("bada hai")
+# else:
+#     print("chhota hai")
+
+
+# day =input("enter the day=")
+# if day=="monday":
+#     print("rajma rice")
+# elif day == "tuesday":
+#     print("pulaw")
+# else:
+#     print("poha")
+
+
+# varx=300-123
+# varx=100
+# number =input("enter the number")
+# if varx==177:
+    # print("barabar hai")
+# else:
+    # print("barabar nhi h")
+
+
+
+    
+
+# varx=300-127
+# print(varx)
+# vary=100
+# if varx==vary:
+#     print("barrabar hai")
+# else:
+#     print("barabar nhi hai")
+
+
+
+# varx=300-127
+# print(varx)
+# vary=100
+# if varx==vary:
+#     print("equal")
+# else:
+#     print("not equal")
+
+
+# a=int(input("inter the number"))
+# if a<10:
+#     print("10 is smaller")
+# if a>20:
+#     print("20 is smaller")
+# else:
+#     print("20 is bigger")
+
+
+# a=int(input("enter the number"))
+# if a<10:
+#     print("10 is smaller")
+# if a>20:
+#     print("20 is smaller")
+# else:
+#     print("20 is  bigger")
+
+
+
+# number=44*30
+# print(number)
+# # num2=1320
+    #    or
+# # num2=1000
+# if number<=num2:
+#     print("greater then or equal")
+# else:
+#     print("lesser")
+
+
+
+# water=int(input("enter the no"))
+# if water<=1:
+#     print("need to be fill")
+# elif water>1 and water<10:
+#     print("not need to fill")
+# else:
+#     print("water will overflow")
+
+
+
+
+
+
+    
