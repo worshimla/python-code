e96f6210234643dcdb87b8b47cbccbaf9cfbead0
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:56:07 +0530

    Add files via upload

---
+++ equilateral_isosceles.py
@@ -0,0 +1,9 @@
+# a=int(input("enter the sides1"))
+# b=int(input("enter the sides2"))
+# c=int(input("enter the sides3"))
+# if a==b==c:
+#     print("it is an equilateral triangle")
+# elif a==b or b==c or c==a:
+#     print("isoceles triangle")
+# else:
+#     print("scalene triangle")
