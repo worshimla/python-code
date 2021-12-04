d1feac6c4a5640bc7c38e394243de76b25d7c9be
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:47:45 +0530

    Add files via upload

---
+++ marks_grade_below 25.py
@@ -0,0 +1,15 @@
+# marks= int(input("enter the mark"))
+# if marks <25:
+#     print("grade F")
+# elif marks>25 and marks<45:
+#     print("grade E")
+# elif marks >45 and marks <50:
+#     print("grade D")
+# elif marks >50 and marks<60:
+#     print("grade C")
+# elif marks >60 and marks <80:
+#     print("grade B")
+# elif marks>80 and marks<=100:
+#     print("grade A")
+# else:
+#     print("no grade")9
