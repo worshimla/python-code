c360032af16d00ae7df4f7f0ac1fd9af276d427b
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:42:25 +0530

    Add files via upload

---
+++ question_4.py
@@ -0,0 +1,32 @@
+# a=int(input("enter the first number"))
+# b=int(input("enter the second number"))
+# if a>b:
+#     print("b is miximum")
+# if b>a:
+#     print("b is miximum")
+# if a==b:
+#     print(" both are equal")
+
+
+# mark=int(input("enter the grade")) 
+# if mark<=20:
+#     print('grade A')
+# elif mark<=20 and mark>=40:
+#     print("grade B")
+# elif mark<40 and mark>=60:
+#     print("grade C")
+# else:
+#     print("above")    
+
+# marks=int(input("enter the number"))
+# if marks<=20:
+#    print("grade a")
+# elif marks<=40:
+#     print("grade b")
+# elif marks<=60:
+#     print("grade c")
+# else:
+    # print("nothing")
+
+
+
