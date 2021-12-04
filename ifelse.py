33766a0785f43b64d28435d4cafffec9c9a6c3f9
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:50:19 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,32 @@
+# varx=int(input("enter any no"))
+# if  varx%2==0:
+#     print("div by 2")
+# else:
+#     print("not div by 2")
+
+
+
+# varx=int(input("enter first no"))
+# vary=int(input("enter second no"))
+# if  varx%vary:
+#     print("divisible")
+# else:
+#     print("not divisible")
+
+
+# a=int(input("enter the number"))
+# b=int(input("enter the number"))
+# c=int(input("enter the number"))
+# if(c<b)and(c<a):
+#     print("c is minimum")
+# elif(b<a)and(b<c):
+#     print("b is minimum")
+# elif(a<b)and(a<c):
+#     # print("a is minimum")
+
+# alp=input("enter the alphabet")
+# if alp in('a','e','i','o','u','A','E','I','O','U'):
+#     print("is a vowel")
+# else:
+#     print("is a consonant")
+ 
