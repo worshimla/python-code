79071184908693ccecbf604516fd67d1f4eb9dec
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:40:42 +0530

    Add files via upload

---
+++ sedule_nested.py
@@ -0,0 +1,23 @@
+time=input("enter the time:")
+if time<="8:00pm":
+    if time>="7:59am":
+        if time<="8:29am":
+            print("breakfast")
+    else:
+        print("not breakfast")
+elif time>="8:00pm":
+    if time<="12:59pm":
+        print("first coding class")
+             
+else:
+    print("99999")
+    
+
+    
+# elif time >="8:00am":
+#     if time<="12:29pm":
+#         print("first coding")
+#     else:
+#         print("no coding")
+# else:
+#     print("99999999999999999")
