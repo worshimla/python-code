284b4d4197aa48fb125add6bdbce9aeb5fe33c6f
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:44:15 +0530

    Add files via upload

---
+++ physic_chemistry.py
@@ -0,0 +1,21 @@
+# phy=int(input("enter the physic mark"))
+# chem= int(input("enter the chemistry mark"))
+# bio=int(input("enter the biology mark"))
+# maths=int(input("enter the maths mark"))
+# comp=int(input("enter the computer mark"))
+# total=(phy+chem+bio+maths+comp)
+# print("total marks",total)
+# per=(total/500)*100
+# print("per",per)
+# if per>=90:
+    # print("grade A")
+# elif per>=80:
+    # print("grade B")
+# elif per>=70:
+    # print("grade C")
+# elif per>=60:
+    # print("grade D")
+# elif per>=50:
+    # print("grade E")
+# else:
+    # print("grade F")
