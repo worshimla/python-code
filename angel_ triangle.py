149d624f6f6e5fcc6e82677136761df1ae2da060
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 22:09:47 +0530

    Add files via upload

---
+++ angel_ triangle.py
@@ -0,0 +1,7 @@
+# angle=int(input("enter the number"))
+# angle1=int(input("enter the number"))
+# angle2=int(input("enter the number"))
+# if angle==angle1 and angle1==angle2 and angle2==angle:
+#     print("valid")
+# else:
+#     print("not valid")    
