f2edd8485efc5adaca2b0e509a934562c83f7276
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:37:20 +0530

    Add files via upload

---
+++ simple_ ifelse.py
@@ -0,0 +1,48 @@
+# year=int(input("enter the year"))
+# if year%4==0:
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+# num =int(input("enter the no "))
+# if num%5==0:
+#     print("divisible by 5")
+# elif num%11==0:
+#     print("divisible by 11 ")
+# else:
+#     print("is not divisible
+
+
+
+
+
+
+
+
+
+# ch=input("enter the character")
+# if ch in "a to z" or "A to Z":
+#     print("alphabtes")
+# elif ch in "0 to 9":
+#     print("digits")
+# else:
+#     print("special character")
+
+
+
+
+
+
+# num1=int(input("enter the num"))
+# num2=int(input("enter the num"))
+# num3=int(input("enter the num"))
+# if num1==num2==num3:
+#     print("triangle valid")
+# else:
+#     print("not vaild") uynum1=int(input("enter the num"))
+# num2=int(input("enter the num"))
+# num3=int(input("enter the num"))
+# if num1==num2==num3:
+#     print("triangle valid")
+# else:
+#     print("not vaild")
