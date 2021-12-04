6f7f879ef662d2a1281c6d4f43bd42729ebcd88d
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:30:49 +0530

    Add files via upload

---
+++ week number.py
@@ -0,0 +1,16 @@
+
+# week=int(input("enter the week no:"))
+# if week==1:
+#     print("monday")
+# elif week==2:
+#     print("tuesday")
+# elif week==3:
+#     print("wednesday")
+# elif week==4:
+#     print("thursday")
+# elif week==5:
+#     print("friday")
+# elif week==6:
+#     print("saturday")
+# else:
+    # print("sunday")
