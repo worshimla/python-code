93c7b30fa07deefb267a7ab1996cbc0e1c3f0696
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:43:45 +0530

    Add files via upload

---
+++ profit_loss.py
@@ -0,0 +1,12 @@
+# cp=int(input("enter the cost_price:-"))
+# sp=int(input("enter the selling_price:-"))
+
+# if sp>cp:
+#     c=sp-cp
+#     print(c,"profit")
+# elif cp>sp:
+#     b=cp-sp
+#     print(b,"loss")
+
+# else:
+#     print("no profit no loss")
