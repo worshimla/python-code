73565e97e8facb4766a5258f845937ff8b82525e
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:42:54 +0530

    Add files via upload

---
+++ question_3.py
@@ -0,0 +1,16 @@
+
+# a=int(input("Enter the num="))1
+# num=44*30
+# print(num)
+# if a>num:
+#     print("a is grater than")
+# else:
+#     print("num is grater than")
+    
+# number=int(input("enter the number"))
+# a=100
+# b=30
+# if 100>30:
+#     print("bara hai")
+# else:
+#     print("bara nhi h")
