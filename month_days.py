bf02ea34b4e0a8f1c2ced2eeb775082c242f4c70
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 20:46:37 +0530

    Add files via upload

---
+++ month_days.py
@@ -0,0 +1,9 @@
+# month=int(input("enter the num"))
+# if month==2:
+#     print("28 days")
+# elif month==4 or month==6 or month==8 or month==11:
+#     print("30 days")
+# elif month>12 or month<1:
+#     print("31 days")
+# else:
+#     print("chekking the month first")
