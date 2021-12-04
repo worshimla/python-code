e730c9b9cecbb4b278be38bafdffd13d7281fc9d
worshimla <95536623+worshimla@users.noreply.github.com>
2021-12-04 21:00:17 +0530

    Add files via upload

---
+++ day-meal.py
@@ -0,0 +1,22 @@
+# day=input("enter the days:")
+# meal=input("enter the meals:")
+# if day=="monday":
+#     if meal=="breakfast":
+#         print("poori sabji")
+#     elif meal=="lunch":
+#         print("sambhar rice")
+#     elif meal=="dinner":
+#         print("chicken rice")
+# elif day=="tuesday":
+#     if meal=="breakfast":
+#         print("poha")
+#     elif meal=="lunch":
+#         print("rajma rice")
+#     elif meal=="dinner":
+#         print("roti sabji")
+# else:
+#     print("nothing")
+
+
+
+
